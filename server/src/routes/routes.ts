cb54fbec8a970fae28cd73f0ef3fb102947447f4
Nadim Ritter <nadim.ritter@let.ethz.ch>
2024-07-30 16:20:24 +0200

    add some routes

--- server/src/routes/routes.ts
+++ server/src/routes/routes.ts
@@ -1,9 +1,17 @@
-import express, {Router} from 'express';
-import * as groupController from '../controllers/group.controller';
+import express, {Router} from "express";
+import * as quizController from "../controllers/quiz.controller";
+import * as examController from "../controllers/exam.controller";
+import * as constants from "../utils/constants";
+
 
 const router: Router = express.Router();
 
-router.get("/group", groupController.getGroups);
+router.get(constants.QUIZ_ROUTE, quizController.getQuizzes);
+
+
+
+router.get(constants.EXAM_ROUTE, examController.getExams);
+router.get(constants.EXAM_CONFIGURATION_MAP_ROUTE + "/:id", examController.getExamConfigurationMap)
 
 
 export default router;
