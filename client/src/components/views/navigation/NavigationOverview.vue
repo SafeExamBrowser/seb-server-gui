f0721e6d3fd498f9ed36a56aa06c3392739f2fcc
Nadim Ritter <nadim.ritter@let.ethz.ch>
2025-01-16 15:52:46 +0100

    SEBSERV-610 links in navigation overview

--- client/src/components/views/navigation/NavigationOverview.vue
+++ client/src/components/views/navigation/NavigationOverview.vue
@@ -1,7 +1,40 @@
 <template>
     <v-row class="navigation-overview-container">
         <v-col>
-            navigation overview is working
+            <v-sheet 
+                elevation="0"
+                color="primary"
+                class="rounded-lg pa-4">
+
+                <v-row>
+                    <v-col>
+                        <div 
+                            class="text-h6"
+                            role="button"
+                            tabindex="0">
+                            <router-link class="link-color" :to="constants.HOME_PAGE_ROUTE">{{ constants.HOME_PAGE_TITLE }}</router-link>
+                        </div>
+                    </v-col>
+
+                    <v-col>
+                        <div 
+                            class="text-h6"
+                            role="button"
+                            tabindex="0">
+                            <router-link class="link-color" :to="constants.EXAM_ROUTE">{{ constants.EXAMS_OVERVIEW_TITLE }}</router-link>
+                        </div>
+                    </v-col>
+
+                    <v-col>
+                        <div 
+                            class="text-h6"
+                            role="button"
+                            tabindex="0">
+                            <router-link class="link-color" :to="constants.MONITORING_ROUTE">{{ constants.MONITORING_TITLE }}</router-link>
+                        </div>
+                    </v-col>
+                </v-row>
+            </v-sheet>
         </v-col>
     </v-row>
 </template>
@@ -36,6 +69,9 @@
         color: white;
     }
 
+    .link-color {
+        color: white;
+    }
 
 
 </style>
