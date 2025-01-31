0259317a110ee8e3ae7cf00fa8362717ce77c854
Nadim Ritter <nadim.ritter@let.ethz.ch>
2025-01-31 13:57:01 +0100

    comment exam creation on final step

--- client/src/components/views/quiz-import/main-content/MainContentSummary.vue
+++ client/src/components/views/quiz-import/main-content/MainContentSummary.vue
@@ -16,22 +16,22 @@
 
     onBeforeMount(async () => {
 
-        if(quizImportStore.selectedQuiz == null || quizImportStore.selectedExamTemplate == null){
-            return;
-        }
+        // if(quizImportStore.selectedQuiz == null || quizImportStore.selectedExamTemplate == null){
+        //     return;
+        // }
 
         
-        const createExamParams: CreateExamPar = {
-            lmsSetupId: quizImportStore.selectedQuiz.lms_setup_id,
-            lms_setup_id: quizImportStore.selectedQuiz.lms_setup_id,
-            externalId: quizImportStore.selectedQuiz.quiz_id,
-            quiz_id: quizImportStore.selectedQuiz.quiz_id,
-            examTemplateId: quizImportStore.selectedExamTemplate.id,
-            quitPassword: quizImportStore.selectedQuitPassword,
-            supporter: quizImportStore.selectedExamSupervisors.map(userAccountName => userAccountName.modelId)
-        }
+        // const createExamParams: CreateExamPar = {
+        //     lmsSetupId: quizImportStore.selectedQuiz.lms_setup_id,
+        //     lms_setup_id: quizImportStore.selectedQuiz.lms_setup_id,
+        //     externalId: quizImportStore.selectedQuiz.quiz_id,
+        //     quiz_id: quizImportStore.selectedQuiz.quiz_id,
+        //     examTemplateId: quizImportStore.selectedExamTemplate.id,
+        //     quitPassword: quizImportStore.selectedQuitPassword,
+        //     supporter: quizImportStore.selectedExamSupervisors.map(userAccountName => userAccountName.modelId)
+        // }
 
-        console.log(await quizImportWizardViewService.createExam(createExamParams))
+        // console.log(await quizImportWizardViewService.createExam(createExamParams))
 
     });
 
