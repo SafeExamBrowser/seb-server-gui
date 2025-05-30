55cfd86d38b9868dec9802e9e82b26712e055142
Rad14nt <andrei@gimli.ro>
2025-05-30 15:36:28 +0200

    Client: Removed mock implementation for fetching institutions and added real implementation

--- client/src/services/seb-server/api-services/institutionService.ts
+++ client/src/services/seb-server/api-services/institutionService.ts
@@ -1,39 +1,10 @@
 import * as apiService from "@/services/apiService";
-import { StorageItemEnum } from "@/models/StorageItemEnum";
 
 const url: string = "/institution";
 
-const mockInstitutions: Institution[] = [
-    {
-        id: "1",
-        entityType: "INSTITUTION",
-        name: "SEB Server"
-    },
-    {
-        id: "2",
-        entityType: "INSTITUTION",
-        name: "ETH"
-    },
-    {
-        id: "3",
-        entityType: "INSTITUTION",
-        name: "UZH"
-    },
-    {
-        id: "4",
-        entityType: "INSTITUTION",
-        name: "New institution"
-    }
-];
-
 export async function getInstitutions(): Promise<Institution[]> {
-    return new Promise(resolve => setTimeout(() => resolve(mockInstitutions), 200));
-}
-
-export async function getInstitutionss(): Promise<Institution[]> {
     return (await apiService.api.get(url)).data;
 }
-
 
 export async function getInstitution(institutionId: string): Promise<Institution  | any>{
     return (await apiService.api.get(url, { params: {institutionId}})).data;
