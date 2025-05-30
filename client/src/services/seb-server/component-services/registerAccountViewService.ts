4fcd1c449f7e307a3778627380993580e7a81bc2
Rad14nt <andrei@gimli.ro>
2025-05-30 16:34:15 +0200

    Client: Implemented Registration Logic

--- client/src/services/seb-server/component-services/registerAccountViewService.ts
+++ client/src/services/seb-server/component-services/registerAccountViewService.ts
@@ -2,14 +2,31 @@ import * as userAccountService from "@/services/seb-server/api-services/userAcco
 import * as institutionService from "@/services/seb-server/api-services/institutionService";
 
 
-// export async function registerUserAccount(uuid: string, password: string, newPassword: string, confirmNewPassword: string): Promise<UserAccount[] | null>{
-//     try{
-//         return await userAccountService.registerUserAccount(uuid, password, newPassword, confirmNewPassword)
-//
-//     }catch(error){
-//         return null;
-//     }
-// }
+export async function registerUserAccount(
+    institutionId: string,
+    name: string,
+    surname: string,
+    username: string,
+    newPassword: string,
+    confirmNewPassword: string,
+    email?: string,
+    timezone?: string
+): Promise<UserAccount | null> {
+    try {
+        return await userAccountService.registerUserAccount(
+            institutionId,
+            name,
+            surname,
+            username,
+            newPassword,
+            confirmNewPassword,
+            email,
+            timezone
+        );
+    } catch (error) {
+        return null;
+    }
+}
 
 
 export async function getInstitutions(): Promise<Institution[] | null>{
