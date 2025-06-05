6d75da0f37c128b85fd739cafd77f5d44ffecf58
Rad14nt <andrei@gimli.ro>
2025-06-05 10:46:40 +0200

    Client: Created User Table Page and User Create

--- client/src/router/router.ts
+++ client/src/router/router.ts
@@ -28,6 +28,8 @@ import ProctoringApplicationSearchPage from "@/components/views/screen-proctorin
 import ExamsOverviewPage from "@/components/views/screen-proctoring/exams-overview/ExamsOverviewPage.vue";
 import SearchPage from "@/components/views/screen-proctoring/search/SearchPage.vue";
 import { useAuthStore } from "@/stores/authentication/authenticationStore";
+import {CREATE_USER_ACCOUNTS_ROUTE} from "@/utils/constants";
+import CreateUserAccount from "@/components/views/seb-server/accounts/CreateUserAccount.vue";
 
 
 
@@ -168,6 +170,15 @@ const routes: Array<RouteRecordRaw> = [
                 component: UserAccountsPage,
                 meta: {
                     titleKey: "titles.userAccounts"
+                },
+            },
+
+            {
+                path: constants.CREATE_USER_ACCOUNTS_ROUTE,
+                name: "CreateUserAccount",
+                component: CreateUserAccount,
+                meta: {
+                    titleKey: "titles.createUserAccount"
                 },
             },
 
