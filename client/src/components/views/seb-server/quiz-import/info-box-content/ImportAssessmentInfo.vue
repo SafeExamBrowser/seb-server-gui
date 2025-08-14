cdf95c5a5adfbd856a30e2ee80085f4b539f4842
Rad14nt <andrei@gimli.ro>
2025-08-14 16:41:25 +0200

    FE: Fe: Changes to prepare exam select assessment tool

--- client/src/components/views/seb-server/quiz-import/info-box-content/ImportAssessmentInfo.vue
+++ client/src/components/views/seb-server/quiz-import/info-box-content/ImportAssessmentInfo.vue
@@ -1,31 +1,9 @@
 <template>
-    <v-row>
-        <v-col>
-            <v-sheet 
-                elevation="4"
-                class="rounded-lg pl-4 pt-3 pr-4">
-
-                <v-row 
-                    class="fill-height"
-                    align="center"
-                    >
-
-                    <v-col cols="5">
-                        <div class="primary-text-color text-h5 font-weight-bold">
-                            {{translate('quizImportWizard.assessmentTool.title')}}
-                        </div>
-                    </v-col>
-
-                </v-row>
-
-            </v-sheet>
-        </v-col>
-    </v-row>
-
+    <ImportGenericInfo></ImportGenericInfo>
 </template>
 
 <script setup lang="ts">
-    import {translate} from "@/utils/generalUtils";
+    import ImportGenericInfo from "@/components/views/seb-server/quiz-import/info-box-content/ImportGenericInfo.vue";
 
 </script>
 
--- client/src/components/views/seb-server/quiz-import/main-content/ImportAssessmentMain.vue
+++ client/src/components/views/seb-server/quiz-import/main-content/ImportAssessmentMain.vue
@@ -1,12 +1,24 @@
 <template>
+    <div class="h-100 w-100">
+
+        <v-row dense>
+            <v-col>
+                <div class="text-h6 font-weight-bold mb-1">
+                    {{translate('quizImportWizard.assessmentTool.title')}}
+                </div>
+                <div class="mb-10 text-body-2">
+                    {{ translate("quizImportWizard.assessmentTool.description") }}
+                </div>
+            </v-col>
+        </v-row>
+
     <v-row>
-        <v-spacer></v-spacer>
         <v-col cols="4">
             <v-select
                 hide-details
                 :placeholder="translate('quizImportWizard.assessmentTool.title')"
                 density="compact"
-                variant="outlined"  
+                variant="outlined"
                 v-model="quizImportStore.selectedAssessmentTool"
                 :items="quizImportStore.availableAssessmentTools?.content"
                 item-value="id"
@@ -15,6 +27,7 @@
         </v-col>
         <v-spacer></v-spacer>
     </v-row>
+    </div>
 
 </template>
 
