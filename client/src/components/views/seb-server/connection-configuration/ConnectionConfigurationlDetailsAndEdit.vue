f2c47a1540d0e072eb2b87a939fe24ac9f38b61d
Rad14nt <andrei@gimli.ro>
2025-08-25 10:27:03 +0200

    FE: Changed url validation to be on Blur

--- client/src/components/views/seb-server/connection-configuration/ConnectionConfigurationlDetailsAndEdit.vue
+++ client/src/components/views/seb-server/connection-configuration/ConnectionConfigurationlDetailsAndEdit.vue
@@ -223,7 +223,7 @@
                                                             variant="outlined"
                                                             v-model="fallbackStartUrl"
                                                             :rules="[requiredIfFallbackRule, urlIfFallbackRule]"
-                                                            validate-on="input"
+                                                            validate-on="blur"
                                                         />
                                                     </v-col>
 
