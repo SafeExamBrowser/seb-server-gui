fcf30f60abd17d5f79c9f5564aa1c5ee0ecba745
Alain Horner <alain@hornerdigital.ch>
2025-09-09 14:02:01 -0600

    harmonise code quality tools (eslint, prettier, editorconfig) -> clean and consistent codebase

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -10,19 +10,20 @@ module.exports = {
     node: true,
   },
   extends: [
-    'vuetify',
-    '@vue/eslint-config-typescript',
-    './.eslintrc-auto-import.json',
+    "vuetify",
+    "@vue/eslint-config-typescript",
+    "./.eslintrc-auto-import.json",
+    "prettier",
   ],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    '@typescript-eslint/ban-ts-comment': [
-      'error',
-      {'ts-ignore': 'allow-with-description'},
-    ],  
-    'no-useless-catch': 'off',
-    'no-undef': 'off',
-    "@typescript-eslint/no-inferrable-types": "off"
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "@typescript-eslint/ban-ts-comment": [
+      "error",
+      { "ts-ignore": "allow-with-description" },
+    ],
+    "no-useless-catch": "off",
+    "no-undef": "off",
+    "@typescript-eslint/no-inferrable-types": "off",
   },
-}
+};
