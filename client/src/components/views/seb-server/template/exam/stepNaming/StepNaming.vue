22dca2a3b24ec305af3546d945a11b65645057c6
Alain Horner <alain@hornerdigital.ch>
2025-09-22 11:39:52 -0600

    demo for very long card content

--- client/src/components/views/seb-server/template/exam/stepNaming/StepNaming.vue
+++ client/src/components/views/seb-server/template/exam/stepNaming/StepNaming.vue
@@ -1,4 +1,9 @@
 <template>
     <!-- TODO @alain: implement -->
-    <div>Step Naming</div>
+    <div>
+        <span>Step Naming</span>
+        <div style="height: 2000px; background: salmon">
+            Demo: Very long content
+        </div>
+    </div>
 </template>
