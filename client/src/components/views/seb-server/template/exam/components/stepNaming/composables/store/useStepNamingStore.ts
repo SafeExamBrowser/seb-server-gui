511f77f51af041b15d9d90ebe1b1dd8b3f1a933a
Alain Horner <alain@hornerdigital.ch>
2025-09-29 14:47:09 -0600

    SEBSERV-715: enum already contains the undefined value

--- client/src/components/views/seb-server/template/exam/components/stepNaming/composables/store/useStepNamingStore.ts
+++ client/src/components/views/seb-server/template/exam/components/stepNaming/composables/store/useStepNamingStore.ts
@@ -5,7 +5,7 @@ import { ref, computed } from "vue";
 export const useStepNamingStore = defineStore("stepNaming", () => {
     const name = ref("");
     const description = ref("");
-    const examType = ref<ExamTypeEnum>();
+    const examType = ref<ExamTypeEnum>(ExamTypeEnum.UNDEFINED);
     const sebClientTemplate = ref<string>();
     const connectionConfiguration = ref<string>();
     const assesmentToolIntegration = ref<boolean>(true);
