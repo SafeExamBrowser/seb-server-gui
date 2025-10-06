ac721c72f00101dca0dcfabaef6d0e5f3fb250ec
Alain Horner <alain@hornerdigital.ch>
2025-10-06 13:06:45 -0600

    SEBSERV-715: rm debug-code

--- client/src/components/views/seb-server/template/exam/components/stepSupervisors/composables/store/useStepSupervisorsStore.ts
+++ client/src/components/views/seb-server/template/exam/components/stepSupervisors/composables/store/useStepSupervisorsStore.ts
@@ -2,15 +2,11 @@ import { defineStore } from "pinia";
 import { ref } from "vue";
 
 const initialState = {
-    isReady: false,
+    isReady: true,
 };
 
 export const useStepSupervisorsStore = defineStore("stepSupervisors", () => {
     const isReady = ref(initialState.isReady);
-
-    const markAsReady = () => {
-        isReady.value = true;
-    };
 
     const $reset = () => {
         isReady.value = initialState.isReady;
@@ -18,7 +14,6 @@ export const useStepSupervisorsStore = defineStore("stepSupervisors", () => {
 
     return {
         isReady,
-        markAsReady,
         $reset,
     };
 });
