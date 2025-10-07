819f6cf12fb5c184ac4368b442709457872eb75b
Alain Horner <alain@hornerdigital.ch>
2025-10-07 14:12:50 -0600

    SEBSERV-715: groups: make edit action standalone

---
+++ client/src/components/views/seb-server/template/exam/components/stepClientGroup/components/ClientGroupEdit.vue
@@ -0,0 +1,53 @@
+<template>
+    <!-- TODO @alain: i18n for button label -->
+    <v-btn
+        icon="mdi-pencil"
+        color="medium-emphasis"
+        variant="text"
+        density="compact"
+        size="small"
+        title="Edit"
+        aria-label="Edit"
+        @click="handleOpenDialog"
+    ></v-btn>
+
+    <v-dialog v-model="dialogOpen" width="auto" @close="handleCancel">
+        <!-- TODO @alain: i18n for title -->
+        <v-card title="Edit group">
+            <template #text>
+                <!-- TODO @alain: add form here -->
+                <div>This is the form</div>
+            </template>
+            <template #actions>
+                <!-- TODO @alain: i18n for button labels -->
+                <v-btn text="Cancel" @click="handleCancel"></v-btn>
+                <v-btn text="Save" @click="handleUpdate"></v-btn>
+            </template>
+        </v-card>
+    </v-dialog>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import { ClientGroup } from "@/components/views/seb-server/template/exam/components/stepClientGroup/types";
+
+const props = defineProps<{
+    item: ClientGroup;
+}>();
+
+const dialogOpen = ref(false);
+
+const handleOpenDialog = () => {
+    dialogOpen.value = true;
+};
+
+const handleCancel = () => {
+    dialogOpen.value = false;
+};
+
+const handleUpdate = () => {
+    // TODO @alain: update group in store
+    console.log("update group:", props.item);
+    dialogOpen.value = false;
+};
+</script>
--- client/src/components/views/seb-server/template/exam/components/stepClientGroup/components/TableItemActions.vue
+++ client/src/components/views/seb-server/template/exam/components/stepClientGroup/components/TableItemActions.vue
@@ -1,16 +1,7 @@
 <template>
     <div class="d-flex ga-2 justify-end">
         <!-- TODO @alain: i18n for button labels -->
-        <v-btn
-            icon="mdi-pencil"
-            color="medium-emphasis"
-            variant="text"
-            density="compact"
-            size="small"
-            title="Edit"
-            aria-label="Edit"
-            @click="handleEditClick(item)"
-        ></v-btn>
+        <ClientGroupEdit :item="item" />
         <v-btn
             icon="mdi-delete"
             color="medium-emphasis"
@@ -27,13 +18,9 @@
 <script setup lang="ts">
 import { ClientGroup } from "@/components/views/seb-server/template/exam/components/stepClientGroup/types";
 import { useStepClientGroupStore } from "@/components/views/seb-server/template/exam/components/stepClientGroup/composables/store/useStepClientGroupStore";
+import ClientGroupEdit from "@/components/views/seb-server/template/exam/components/stepClientGroup/components/ClientGroupEdit.vue";
 
 const { deleteGroup } = useStepClientGroupStore();
-
-const handleEditClick = (item: ClientGroup) => {
-    // TODO @alain: implement edit form
-    console.log("edit", item);
-};
 
 const handleDeleteClick = (item: ClientGroup) => {
     deleteGroup(item);
