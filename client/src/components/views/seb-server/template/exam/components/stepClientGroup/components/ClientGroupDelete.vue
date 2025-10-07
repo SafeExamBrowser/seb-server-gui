a12c9f920595a003e553b3a454c6fb38b4ee4855
Alain Horner <alain@hornerdigital.ch>
2025-10-07 14:29:38 -0600

    SEBSERV-715: groups: make delete action standalone

---
+++ client/src/components/views/seb-server/template/exam/components/stepClientGroup/components/ClientGroupDelete.vue
@@ -0,0 +1,28 @@
+<template>
+    <!-- TODO @alain: i18n for button label -->
+    <v-btn
+        icon="mdi-delete"
+        color="medium-emphasis"
+        variant="text"
+        density="compact"
+        size="small"
+        title="Delete"
+        aria-label="Delete"
+        @click="handleDeleteClick"
+    ></v-btn>
+</template>
+
+<script setup lang="ts">
+import { useStepClientGroupStore } from "@/components/views/seb-server/template/exam/components/stepClientGroup/composables/store/useStepClientGroupStore";
+import { ClientGroup } from "@/components/views/seb-server/template/exam/components/stepClientGroup/types";
+
+const props = defineProps<{
+    item: ClientGroup;
+}>();
+
+const { deleteGroup } = useStepClientGroupStore();
+
+const handleDeleteClick = () => {
+    deleteGroup(props.item);
+};
+</script>
--- client/src/components/views/seb-server/template/exam/components/stepClientGroup/components/TableItemActions.vue
+++ client/src/components/views/seb-server/template/exam/components/stepClientGroup/components/TableItemActions.vue
@@ -1,30 +1,12 @@
 <template>
     <div class="d-flex ga-2 justify-end">
-        <!-- TODO @alain: i18n for button labels -->
         <ClientGroupEdit :item="item" />
-        <v-btn
-            icon="mdi-delete"
-            color="medium-emphasis"
-            variant="text"
-            density="compact"
-            size="small"
-            title="Delete"
-            aria-label="Delete"
-            @click="handleDeleteClick(item)"
-        ></v-btn>
+        <ClientGroupDelete :item="item" />
     </div>
 </template>
 
 <script setup lang="ts">
 import { ClientGroup } from "@/components/views/seb-server/template/exam/components/stepClientGroup/types";
-import { useStepClientGroupStore } from "@/components/views/seb-server/template/exam/components/stepClientGroup/composables/store/useStepClientGroupStore";
-import ClientGroupEdit from "@/components/views/seb-server/template/exam/components/stepClientGroup/components/ClientGroupEdit.vue";
-
-const { deleteGroup } = useStepClientGroupStore();
-
-const handleDeleteClick = (item: ClientGroup) => {
-    deleteGroup(item);
-};
 
 defineProps<{
     item: ClientGroup;
