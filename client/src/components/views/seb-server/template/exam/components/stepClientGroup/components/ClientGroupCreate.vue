b3fac99ac77be14e7c5ae3addb4779dc0e8ed993
Alain Horner <alain@hornerdigital.ch>
2025-10-21 11:38:10 -0600

    SEBSERV-715: disallow creating new groups until the conditions are met

--- client/src/components/views/seb-server/template/exam/components/stepClientGroup/components/ClientGroupCreate.vue
+++ client/src/components/views/seb-server/template/exam/components/stepClientGroup/components/ClientGroupCreate.vue
@@ -1,5 +1,6 @@
 <template>
     <ClientGroupDialog
+        :disabled="!allowCreateNewClientGroup"
         icon-activator="mdi-plus-circle-outline"
         color-activator="primary"
         :label-activator="$t('clientGroups.addDialogTitle')"
@@ -16,8 +17,23 @@ import {
     useStepClientGroupStore,
 } from "@/components/views/seb-server/template/exam/components/stepClientGroup/composables/store/useStepClientGroupStore";
 import { ClientGroup } from "@/components/views/seb-server/template/exam/components/stepClientGroup/types";
+import { storeToRefs } from "pinia";
+import { useScreenProctoringStore } from "@/components/views/seb-server/template/exam/composables/store/useScreenProctoringStore";
+import { computed } from "vue";
 
 const { createGroup } = useStepClientGroupStore();
+
+const {
+    enabled: screenProctoringEnabled,
+    collectionStrategy: screenProctoringCollectionStrategy,
+} = storeToRefs(useScreenProctoringStore());
+
+const allowCreateNewClientGroup = computed<boolean>(() => {
+    return !(
+        screenProctoringEnabled.value &&
+        screenProctoringCollectionStrategy.value === undefined
+    );
+});
 
 const getClientGroup = () => {
     return getEmptyClientGroup();
