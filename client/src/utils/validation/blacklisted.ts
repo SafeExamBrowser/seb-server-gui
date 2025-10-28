32cc68de6dbde8a4d05991619b752cf8bc8fac52
Alain Horner <alain@hornerdigital.ch>
2025-10-28 12:21:07 -0600

    SEBSERV-715: blacklist validation: BE considers two strings equal irrespective of case, so we do the same

--- client/src/utils/validation/blacklisted.ts
+++ client/src/utils/validation/blacklisted.ts
@@ -1,8 +1,12 @@
 import i18n from "@/i18n";
 
 export const blacklisted = (blacklistedValues: Set<string>, err?: string) => {
+    const blacklistedValuesLowerCase = new Set(
+        Array.from(blacklistedValues).map((v) => v.toLowerCase()),
+    );
+
     return (v: string) =>
-        !blacklistedValues.has(v) ||
+        !blacklistedValuesLowerCase.has(v.toLowerCase()) ||
         err ||
         i18n.global.t("general.validation.blacklisted", {
             values: Array.from(blacklistedValues).join(", "),
