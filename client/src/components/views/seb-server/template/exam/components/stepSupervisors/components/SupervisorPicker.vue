803b5d05092257f51fc1cbe7e7e67c4f7f7bd5a1
Alain Horner <alain@hornerdigital.ch>
2025-10-28 14:54:39 -0600

    SEBSERV-715: selection and unselection

--- client/src/components/views/seb-server/template/exam/components/stepSupervisors/components/SupervisorPicker.vue
+++ client/src/components/views/seb-server/template/exam/components/stepSupervisors/components/SupervisorPicker.vue
@@ -1,9 +1,17 @@
 <template>
-    <SupervisorList
-        v-if="supervisorsAvailable"
-        :supervisors="supervisorsAvailable"
-        @select="handleSupervisorSelected"
-    />
+    <!-- TODO @alain: proper layout -->
+    <div>
+        <h3>Available supervisors:</h3>
+        <SupervisorList
+            :supervisors="supervisorsAvailable"
+            @select="handleSupervisorSelected"
+        />
+        <h3>Selected supervisors:</h3>
+        <SupervisorList
+            :supervisors="supervisorsSelected"
+            @select="handleSupervisorUnselected"
+        />
+    </div>
 </template>
 
 <script setup lang="ts">
@@ -14,25 +22,41 @@ const props = defineProps<{
     supervisors: UserAccountName[];
 }>();
 
-const superVisorIdsSelected = defineModel<string[]>({
+const supervisorIdsSelected = defineModel<string[]>({
     required: true,
 });
 
 const supervisorsAvailable = computed(() =>
     props.supervisors.filter(
         (supervisor) =>
-            !superVisorIdsSelected.value.includes(supervisor.modelId),
+            !supervisorIdsSelected.value.includes(supervisor.modelId),
+    ),
+);
+
+const supervisorsSelected = computed(() =>
+    props.supervisors.filter((supervisor) =>
+        supervisorIdsSelected.value.includes(supervisor.modelId),
     ),
 );
 
 const handleSupervisorSelected = (supervisorId: string) => {
-    if (superVisorIdsSelected.value.includes(supervisorId)) {
+    if (supervisorIdsSelected.value.includes(supervisorId)) {
         throw new Error("Supervisor already selected");
     }
 
-    superVisorIdsSelected.value = [
-        ...superVisorIdsSelected.value,
+    supervisorIdsSelected.value = [
+        ...supervisorIdsSelected.value,
         supervisorId,
+    ];
+};
+
+const handleSupervisorUnselected = (supervisorId: string) => {
+    if (!supervisorIdsSelected.value.includes(supervisorId)) {
+        throw new Error("Supervisor not selected");
+    }
+
+    supervisorIdsSelected.value = [
+        ...supervisorIdsSelected.value.filter((id) => id !== supervisorId),
     ];
 };
 </script>
