937ac07c47ae3829582b7002b64153e879c8a134
Alain Horner <alain@hornerdigital.ch>
2025-11-04 12:00:06 -0600

    SEBSERV-715: spacing

--- client/src/components/views/seb-server/template/exam/components/stepSupervisors/components/supervisorPicker/components/ListHeader.vue
+++ client/src/components/views/seb-server/template/exam/components/stepSupervisors/components/supervisorPicker/components/ListHeader.vue
@@ -1,5 +1,7 @@
 <template>
-    <h4 class="text-subtitle-1 font-weight-medium flex-grow-0 flex-shrink-0">
+    <h4
+        class="text-subtitle-1 font-weight-medium flex-grow-0 flex-shrink-0 mb-2"
+    >
         {{ label }}
     </h4>
 </template>
