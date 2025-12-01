f3335cb749301c556748a09b11948f620ce6c2f8
Alain Horner <alain@hornerdigital.ch>
2025-12-01 13:03:10 -0600

    SEBSERV-715: move color formfield to its own file

--- client/src/components/widgets/formBuilder/FormBuilder.vue
+++ client/src/components/widgets/formBuilder/FormBuilder.vue
@@ -46,19 +46,14 @@
                 }"
             >
             </v-number-input>
-            <v-color-input
+            <FormFieldColor
                 v-else-if="field.type === 'color'"
                 v-model="field.model.value"
-                v-bind="{
+                :standard-properties="{
                     ...getBaseProperties(field),
                     ...getTextualProperties(field),
                 }"
-                mode="hex"
-                pip-location="append-inner"
-                color-pip
-                @keydown="handleColorInputKeydown"
-            >
-            </v-color-input>
+            />
             <v-select
                 v-else-if="field.type === 'select'"
                 v-model="field.model.value"
@@ -103,6 +98,7 @@ import {
 import { VInput } from "vuetify/components";
 import { ref, nextTick, watch } from "vue";
 import FormFieldCollection from "./components/FormFieldCollection.vue";
+import FormFieldColor from "@/components/widgets/formBuilder/components/FormFieldColor.vue";
 
 const isValid = defineModel<boolean | null>();
 
@@ -221,27 +217,5 @@ const handleFieldValueUpdated = async (fieldName: string) => {
 
 const handleSubmit = async () => {
     emit("submit");
-};
-
-const allowColorInputKeydown = (e: KeyboardEvent) => {
-    // allow basic navigation
-    if (["Enter", "Escape", "Tab"].includes(e.key)) {
-        return true;
-    }
-
-    // allow copy to clipboard
-    if (["KeyC"].includes(e.code) && (e.ctrlKey || e.metaKey)) {
-        return true;
-    }
-
-    return false;
-};
-
-const handleColorInputKeydown = (e: KeyboardEvent) => {
-    // This ensures, that basic keyboard navigation & copying to the clipboard works while not allowing the user to type in the input field itself.
-    // This is not ideal, but it's the only way I found to make the color input `readonly` while still allowing the color picker to be opened.
-    if (!allowColorInputKeydown(e)) {
-        e.preventDefault();
-    }
 };
 </script>
---
+++ client/src/components/widgets/formBuilder/components/FormFieldColor.vue
@@ -0,0 +1,44 @@
+<template>
+    <v-color-input
+        v-model="model"
+        v-bind="standardProperties"
+        mode="hex"
+        pip-location="append-inner"
+        color-pip
+        @keydown="handleColorInputKeydown"
+    >
+    </v-color-input>
+</template>
+
+<script setup lang="ts">
+import { FormFieldBaseProperties, FormFieldTextualProperties } from "../types";
+
+const model = defineModel<string | undefined>();
+
+defineProps<{
+    // field: FormField & { type: "color" };
+    standardProperties: FormFieldBaseProperties & FormFieldTextualProperties;
+}>();
+
+const allowColorInputKeydown = (e: KeyboardEvent) => {
+    // allow basic navigation
+    if (["Enter", "Escape", "Tab"].includes(e.key)) {
+        return true;
+    }
+
+    // allow copy to clipboard
+    if (["KeyC"].includes(e.code) && (e.ctrlKey || e.metaKey)) {
+        return true;
+    }
+
+    return false;
+};
+
+const handleColorInputKeydown = (e: KeyboardEvent) => {
+    // This ensures, that basic keyboard navigation & copying to the clipboard works while not allowing the user to type in the input field itself.
+    // This is not ideal, but it's the only way I found to make the color input `readonly` while still allowing the color picker to be opened.
+    if (!allowColorInputKeydown(e)) {
+        e.preventDefault();
+    }
+};
+</script>
