0bf6ec7d4216e39c7aea979c7d51c1e9f81734ec
Rad14nt <andrei@gimli.ro>
2025-12-01 13:15:25 +0100

    Client: storybook try 200

--- client/.storybook/main.ts
+++ client/.storybook/main.ts
@@ -1,6 +1,11 @@
 import type { StorybookConfig } from "@storybook/vue3-vite";
+import { mergeConfig } from "vite";
 
 const config: StorybookConfig = {
+    framework: {
+        name: "@storybook/vue3-vite",
+        options: {},
+    },
     stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
     addons: [
         "@chromatic-com/storybook",
@@ -8,9 +13,14 @@ const config: StorybookConfig = {
         "@storybook/addon-onboarding",
         "@storybook/addon-a11y",
     ],
-    framework: {
-        name: "@storybook/vue3-vite",
-        options: {},
+    core: {
+        builder: "@storybook/builder-vite",
+    },
+    async viteFinal(config_) {
+        return mergeConfig(config_, {
+            base: "/storybook/",
+        });
     },
 };
+
 export default config;
