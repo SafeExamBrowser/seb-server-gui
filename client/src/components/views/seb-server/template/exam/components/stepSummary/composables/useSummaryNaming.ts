77ab81582fdab7c6b461d7839734a7d18ea576a2
Alain Horner <alain@hornerdigital.ch>
2025-12-02 14:26:21 -0600

    SEBSERV-715: cs

--- client/src/components/views/seb-server/template/exam/components/stepSummary/composables/useSummaryNaming.ts
+++ client/src/components/views/seb-server/template/exam/components/stepSummary/composables/useSummaryNaming.ts
@@ -6,72 +6,70 @@ const emptyValue = "–" as const;
 
 export const useSummaryNaming = (
     examTemplate: ExamTemplate,
-): SummarySectionData => {
-    return {
-        label: i18n.global.t(
-            "createTemplateExam.steps.summary.sections.naming.title",
-        ),
-        items: [
-            {
-                type: "basic",
-                key: "name",
-                label: i18n.global.t(
-                    "createTemplateExam.steps.naming.fields.name.label",
-                ),
-                value: examTemplate.name,
-            },
-            {
-                type: "basic",
-                key: "description",
-                label: i18n.global.t(
-                    "createTemplateExam.steps.naming.fields.description.label",
-                ),
-                value: examTemplate.description ?? emptyValue,
-            },
-            {
-                type: "basic",
-                key: "examType",
-                label: i18n.global.t(
-                    "createTemplateExam.steps.naming.fields.examType.label",
-                ),
-                value: examTemplate.examType ?? emptyValue, // TODO @alain: translate type
-            },
-            {
-                type: "basic",
-                key: "clientConfiguration",
-                label: i18n.global.t(
-                    "createTemplateExam.steps.naming.fields.clientConfiguration.label",
-                ),
-                value: examTemplate.clientConfigurationId
-                    ? `${examTemplate.clientConfigurationId}` // TODO @alain: show name instead of id
-                    : emptyValue,
-            },
-            {
-                type: "basic",
-                key: "configurationTemplate",
-                label: i18n.global.t(
-                    "createTemplateExam.steps.naming.fields.configurationTemplate.label",
-                ),
-                value: examTemplate.configurationTemplateId
-                    ? `${examTemplate.configurationTemplateId}` // TODO @alain: show name instead of id
-                    : emptyValue,
-            },
-            {
-                type: "basic",
-                key: "lmsIntegration",
-                label: i18n.global.t(
-                    "createTemplateExam.steps.naming.fields.lmsIntegration.label",
-                ),
-                value: examTemplate.lmsIntegration ? "Yes" : "No", // TODO @alain: bool?
-            },
-            {
-                type: "basic",
-                key: "institutionalDefault",
-                label: i18n.global.t(
-                    "createTemplateExam.steps.naming.fields.institutionalDefault.label",
-                ),
-                value: examTemplate.institutionalDefault ? "Yes" : "No", // TODO @alain: bool?
-            },
-        ],
-    };
-};
+): SummarySectionData => ({
+    label: i18n.global.t(
+        "createTemplateExam.steps.summary.sections.naming.title",
+    ),
+    items: [
+        {
+            type: "basic",
+            key: "name",
+            label: i18n.global.t(
+                "createTemplateExam.steps.naming.fields.name.label",
+            ),
+            value: examTemplate.name,
+        },
+        {
+            type: "basic",
+            key: "description",
+            label: i18n.global.t(
+                "createTemplateExam.steps.naming.fields.description.label",
+            ),
+            value: examTemplate.description ?? emptyValue,
+        },
+        {
+            type: "basic",
+            key: "examType",
+            label: i18n.global.t(
+                "createTemplateExam.steps.naming.fields.examType.label",
+            ),
+            value: examTemplate.examType ?? emptyValue, // TODO @alain: translate type
+        },
+        {
+            type: "basic",
+            key: "clientConfiguration",
+            label: i18n.global.t(
+                "createTemplateExam.steps.naming.fields.clientConfiguration.label",
+            ),
+            value: examTemplate.clientConfigurationId
+                ? `${examTemplate.clientConfigurationId}` // TODO @alain: show name instead of id
+                : emptyValue,
+        },
+        {
+            type: "basic",
+            key: "configurationTemplate",
+            label: i18n.global.t(
+                "createTemplateExam.steps.naming.fields.configurationTemplate.label",
+            ),
+            value: examTemplate.configurationTemplateId
+                ? `${examTemplate.configurationTemplateId}` // TODO @alain: show name instead of id
+                : emptyValue,
+        },
+        {
+            type: "basic",
+            key: "lmsIntegration",
+            label: i18n.global.t(
+                "createTemplateExam.steps.naming.fields.lmsIntegration.label",
+            ),
+            value: examTemplate.lmsIntegration ? "Yes" : "No", // TODO @alain: bool?
+        },
+        {
+            type: "basic",
+            key: "institutionalDefault",
+            label: i18n.global.t(
+                "createTemplateExam.steps.naming.fields.institutionalDefault.label",
+            ),
+            value: examTemplate.institutionalDefault ? "Yes" : "No", // TODO @alain: bool?
+        },
+    ],
+});
