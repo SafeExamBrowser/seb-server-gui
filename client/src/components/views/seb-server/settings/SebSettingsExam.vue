95f250762067d9cddc57a7e282214d15a4fada0a
Andreas Hefti <andreas.hefti@id.ethz.ch>
2025-12-08 16:23:44 +0100

    Exam View

--- client/src/components/views/seb-server/settings/SebSettingsExam.vue
+++ client/src/components/views/seb-server/settings/SebSettingsExam.vue
@@ -1,51 +1,79 @@
-<template>
+<!-- <template>
     <v-row> EXAM Settings TODO </v-row>
 </template>
 
 <script setup lang="ts">
-// import * as sebSettingsService from "@/services/seb-server/component-services/sebSettingsService";
-// import { useI18n } from "vue-i18n";
-// import { stringToBoolean, translate } from "@/utils/generalUtils";
-// import { useSEBSettingsStore } from "@/stores/seb-server/sebSettingsStore";
-// import { ViewType } from "@/models/seb-server/sebSettingsEnums";
-// import { ref, onBeforeMount } from "vue";
+import * as sebSettingsService from "@/services/seb-server/component-services/sebSettingsService";
+import { useI18n } from "vue-i18n";
+import { stringToBoolean, translate } from "@/utils/generalUtils";
+import { useSEBSettingsStore } from "@/stores/seb-server/sebSettingsStore";
+import { ViewType } from "@/models/seb-server/sebSettingsEnums";
+import { ref, onBeforeMount } from "vue";
+import {
+    SEBSettingsValue,
+    SEBSettingsView,
+    SEBSettingAttribute,
+} from "@/models/seb-server/sebSettings";
 
-// const i18n = useI18n();
-// const sebSettingsStore = useSEBSettingsStore();
+const i18n = useI18n();
+const sebSettingsStore = useSEBSettingsStore();
 
-// // the parent component identifier
-// let componentId: string;
 
-// onBeforeMount(async () => {
-//     if (sebSettingsStore.selectedContainerId == null) {
-//         return;
-//     }
+const examSessionClearCookiesOnStart = ref<boolean>(false);
+const examSessionClearCookiesOnEnd = ref<boolean>(false);
 
-//     componentId = sebSettingsStore.selectedContainerId.toString();
+const quitURL = ref<boolean>(false);
+const quitURLConfirm = ref<boolean>(false);
 
-//     const examSettings: SEBSettingsView | null =
-//         await sebSettingsService.getViewSettings(
-//             ViewType.EXAM,
-//             componentId,
-//             sebSettingsStore.isExam,
-//         );
-//     if (examSettings == null) {
-//         return;
-//     }
-// });
+let componentId: string;
+let singleValues: Map<string, SEBSettingsValue>;
 
-// async function saveSingleValue(valId: number, value: string) {
-//     await sebSettingsService.updateSEBSettingValue(
-//         componentId,
-//         valId.toString(),
-//         value,
-//         sebSettingsStore.isExam,
-//     );
-// }
+onBeforeMount(async () => {
+    if (sebSettingsStore.selectedContainerId == null) {
+        return;
+    }
 
-// async function saveOnFocusLost(focusIn: boolean, valId: number, value: string) {
+    componentId = sebSettingsStore.selectedContainerId.toString();
+
+    const examSettings: SEBSettingsView | null =
+        await sebSettingsService.getViewSettings(
+            ViewType.EXAM,
+            componentId,
+            sebSettingsStore.isExam,
+        );
+    if (examSettings == null) {
+        return;
+    }
+
+    singleValues = new Map<string, SEBSettingsValue>(
+        Object.entries(examSettings.singleValues),
+    );
+
+
+});
+
+async function saveSingleValue(name: string, value: string) {
+    const setting: SEBSettingsValue = getSingleValue(name);
+    await sebSettingsService.updateSEBSettingValue(
+        componentId,
+        setting.id.toString(),
+        value,
+        sebSettingsStore.isExam,
+    );
+}
+
+// async function saveOnFocusLost(focusIn: boolean, name: string, value: string) {
 //     if (!focusIn) {
-//         saveSingleValue(valId, value);
+//         saveSingleValue(name, value);
 //     }
 // }
-</script>
+
+function getSingleValue(name: string): SEBSettingsValue {
+    const value = singleValues.get(name);
+    if (!value) {
+        throw new Error("No Single Value" + name + " found");
+    }
+
+    return value;
+}
+</script> -->
