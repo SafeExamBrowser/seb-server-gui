ab54be246817d0942cce0bf3b54c9a4190e10b21
Alain Horner <alain@hornerdigital.ch>
2025-12-09 10:57:55 -0600

    SEBSERV-715: show name instead of id for clientConfiguration and template in summary

--- client/src/components/views/seb-server/template/exam/components/stepSummary/composables/useSummary.ts
+++ client/src/components/views/seb-server/template/exam/components/stepSummary/composables/useSummary.ts
@@ -7,24 +7,58 @@ import { getSummaryNaming } from "@/components/views/seb-server/template/exam/co
 import { getSummaryIndicators } from "@/components/views/seb-server/template/exam/components/stepSummary/helpers/getSummaryIndicators";
 import { getSummarySupervisors } from "@/components/views/seb-server/template/exam/components/stepSummary/helpers/getSummarySupervisors";
 import { useSupervisors } from "@/composables/useSupervisors";
+import { useConfigurationTemplateNames } from "@/composables/useConfigurationTemplateNames";
+import { useClientConfigurationNames } from "@/composables/useClientConfigurationNames";
 
 export const useSummary = (): ComputedRef<SummarySectionData[]> => {
     const examTemplate = useCreateExamTemplateStore().examTemplate;
 
     const {
         data: supervisors,
-        loading,
-        error: errorLoading,
+        loading: loadingSupervisors,
+        error: errorSupervisors,
     } = useSupervisors();
+
+    const {
+        data: configurationTemplateNames,
+        loading: loadingConfigurationTemplateNames,
+        error: errorConfigurationTemplateNames,
+    } = useConfigurationTemplateNames();
+
+    const {
+        data: clientConfigurationNames,
+        loading: loadingClientConfigurationNames,
+        error: errorClientConfigurationNames,
+    } = useClientConfigurationNames();
 
     const summary = computed<SummarySectionData[]>(() => {
         const userAccounts =
-            loading.value || errorLoading.value || !supervisors.value
+            loadingSupervisors.value ||
+            errorSupervisors.value ||
+            !supervisors.value
                 ? []
                 : supervisors.value;
 
+        const configurationTemplates =
+            loadingConfigurationTemplateNames.value ||
+            errorConfigurationTemplateNames.value ||
+            !configurationTemplateNames.value
+                ? []
+                : configurationTemplateNames.value;
+
+        const clientConfigurations =
+            loadingClientConfigurationNames.value ||
+            errorClientConfigurationNames.value ||
+            !clientConfigurationNames.value
+                ? []
+                : clientConfigurationNames.value;
+
         return [
-            getSummaryNaming(examTemplate),
+            getSummaryNaming(
+                examTemplate,
+                configurationTemplates,
+                clientConfigurations,
+            ),
             getSummarySupervisors(examTemplate, userAccounts),
             getSummaryIndicators(examTemplate),
             getSummaryClientGroups(examTemplate),
