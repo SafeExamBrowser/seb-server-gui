ac76c72a6385050f1fb4cc23d20ff7e2163a1e3d
Alain Horner <alain@hornerdigital.ch>
2025-12-09 12:54:13 -0600

    SEBSERV-715: show fallbackGroups in summary

--- client/src/components/views/seb-server/template/exam/components/stepSummary/helpers/getSummaryClientGroups.ts
+++ client/src/components/views/seb-server/template/exam/components/stepSummary/helpers/getSummaryClientGroups.ts
@@ -40,7 +40,7 @@ export const getSummaryClientGroups = (
         return typeDetails;
     };
 
-    const getSummaryClientGroup = (
+    const getClientGroup = (
         clientGroup: ClientGroupTemplate,
         clientGroupIndex: number,
     ) => {
@@ -94,13 +94,85 @@ export const getSummaryClientGroups = (
         };
     };
 
+    const getFallbackGroupTypeValue = () => {
+        if (examTemplate.EXAM_ATTRIBUTES.spsCollectingStrategy === "EXAM") {
+            return i18n.global.t(
+                "createTemplateExam.steps.clientGroup.fields.type.types.SCREEN_PROCTORING_SINGLE",
+            );
+        }
+
+        if (
+            examTemplate.EXAM_ATTRIBUTES.spsCollectingStrategy ===
+            "APPLY_SEB_GROUPS"
+        ) {
+            return i18n.global.t(
+                "createTemplateExam.steps.clientGroup.fields.type.types.SCREEN_PROCTORING_FALLBACK",
+            );
+        }
+
+        // TODO @alain: look into this again when we have stricter API types.
+        // If our spsCollectionStrategy is more stricly defined (i.e. specific values instead of any string), we can remove this fallback.
+        return i18n.global.t("createTemplateExam.steps.summary.notFoundValue");
+    };
+
+    const getClientGroupFallback = () => {
+        if (!examTemplate.EXAM_ATTRIBUTES.spsCollectingGroupName) {
+            return undefined;
+        }
+
+        const items: (SummarySectionItem & { type: "basic" })[] = [
+            {
+                type: "basic" as const,
+                key: "name",
+                label: i18n.global.t(
+                    "createTemplateExam.steps.clientGroup.fields.name.label",
+                ),
+                value: {
+                    type: "string",
+                    value: examTemplate.EXAM_ATTRIBUTES.spsCollectingGroupName,
+                },
+            },
+            {
+                type: "basic" as const,
+                key: "typeDetails",
+                label: i18n.global.t(
+                    "createTemplateExam.steps.clientGroup.fields.type.label",
+                ),
+                value: {
+                    type: "string",
+                    value: getFallbackGroupTypeValue(),
+                },
+            },
+            {
+                type: "basic" as const,
+                key: "screenProctoringEnabled",
+                label: i18n.global.t(
+                    "createTemplateExam.steps.clientGroup.fields.screenProctoringEnabled.label",
+                ),
+                value: {
+                    type: "boolean",
+                    value: true,
+                },
+            },
+        ];
+
+        return {
+            type: "collection" as const,
+            key: `clientGroup-fallback`,
+            items,
+        };
+    };
+
     return {
         label: i18n.global.t(
             "createTemplateExam.steps.summary.sections.clientGroup.title",
         ),
-        items: examTemplate.CLIENT_GROUP_TEMPLATES.map(
-            (clientGroup, clientGroupIndex) =>
-                getSummaryClientGroup(clientGroup, clientGroupIndex),
-        ),
+        items: [
+            ...examTemplate.CLIENT_GROUP_TEMPLATES.map(
+                (clientGroup, clientGroupIndex) =>
+                    getClientGroup(clientGroup, clientGroupIndex),
+            ),
+            getClientGroupFallback(),
+        ].filter((item) => item !== undefined),
     };
 };
