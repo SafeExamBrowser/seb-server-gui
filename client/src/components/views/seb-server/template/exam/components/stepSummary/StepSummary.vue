da165d986a8443bc747c13710b051b22160aa381
Alain Horner <alain@hornerdigital.ch>
2025-12-08 18:44:50 -0600

    SEBSERV-715: naming

--- client/src/components/views/seb-server/template/exam/components/stepSummary/StepSummary.vue
+++ client/src/components/views/seb-server/template/exam/components/stepSummary/StepSummary.vue
@@ -3,13 +3,13 @@
         :title="$t('createTemplateExam.steps.summary.title')"
         :subtitle="$t('createTemplateExam.steps.summary.subtitle')"
     >
-        <SummarySections :sections="summarySections" />
+        <WizardSummary :sections="summarySections" />
     </StepItem>
 </template>
 <script setup lang="ts">
 import StepItem from "@/components/widgets/stepItem/StepItem.vue";
 import { useSummary } from "@/components/views/seb-server/template/exam/components/stepSummary/composables/useSummary";
-import SummarySections from "@/components/views/seb-server/template/exam/components/stepSummary/components/SummarySections.vue";
+import WizardSummary from "@/components/views/seb-server/template/exam/components/stepSummary/components/WizardSummary.vue";
 
 const summarySections = useSummary();
 </script>
