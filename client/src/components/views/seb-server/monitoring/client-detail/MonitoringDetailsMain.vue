a1cb4d55a8ea70fcd48e75ffa4c5e99700e3ca51
Andreas Hefti <andreas.hefti@id.ethz.ch>
2026-01-08 13:19:50 +0100

    SEBSERV-830 reinvent old ProctoringViewPage due to flickering (fullscreen)

--- client/src/components/views/seb-server/monitoring/client-detail/MonitoringDetailsMain.vue
+++ client/src/components/views/seb-server/monitoring/client-detail/MonitoringDetailsMain.vue
@@ -109,7 +109,7 @@
 
                 <!-- Proctoring view -->
                 <div v-if="currentView === 'proctoring'" class="view-container">
-                    <ProctoringViewPage :session-id-prop="connectionToken" />
+                    <ProctoringViewPageOld :session-id-prop="connectionToken" />
                 </div>
 
                 <!-- Logs view -->
@@ -131,7 +131,7 @@ import { ref, computed, watch, nextTick } from "vue";
 import { useRoute } from "vue-router";
 import type { ComputedRef } from "vue";
 import { ClientNotification } from "@/models/seb-server/monitoring";
-import ProctoringViewPage from "@/components/views/screen-proctoring/proctoring/ProctoringViewPage.vue";
+import ProctoringViewPageOld from "@/components/views/screen-proctoring/proctoring/ProctoringViewPageOld.vue";
 import MonitoringClientLogsContainer from "@/components/views/seb-server/monitoring/client-detail/MonitoringClientLogsContainer.vue";
 
 // route params
