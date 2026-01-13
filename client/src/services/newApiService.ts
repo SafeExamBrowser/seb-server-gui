5d25603ef55827f2e5dec7c90a50dd65cb2e6185
Alain Horner <alain@hornerdigital.ch>
2026-01-13 14:32:30 -0600

    SEBSERV-832: fix type

--- client/src/services/newApiService.ts
+++ client/src/services/newApiService.ts
@@ -1,6 +1,6 @@
 // TODO @alain: this will eventually replace the apiService.ts file
 
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 import * as ENV from "@/config/envConfig";
 import { useAuthStore } from "@/stores/authentication/authenticationStore";
 import { StorageItemEnum } from "@/models/StorageItemEnum";
@@ -9,10 +9,7 @@ const api = axios.create({
     baseURL: `${ENV.SERVER_URL}:${3002}`, // TODO @alain: don't hardcode port (this is the port of the new proxy server)
 });
 
-export const get = (
-    url: string,
-    options?: { params?: Record<string, string> },
-) => {
+export const get = (url: string, options?: AxiosRequestConfig) => {
     const authStore = useAuthStore();
 
     return api.get(url, {
