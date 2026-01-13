910c60aedaaa07a3476add906b5dcf77440da8a9
Alain Horner <alain@hornerdigital.ch>
2026-01-12 18:03:58 -0600

    SEBSERV-832: logging

--- proxy/src/index.ts
+++ proxy/src/index.ts
@@ -2,6 +2,7 @@ import { createProxyServer } from "http-proxy-3";
 import http from "http";
 import { setCorsHeaders } from "./utils/cors";
 import { parseEnv } from "./utils/env";
+import { logInfo, logRequest } from "./utils/logger";
 
 const API_PREFIX = "/admin-api/v1";
 
@@ -18,6 +19,7 @@ proxyConfig.on("proxyReq", (proxyReq, req, res) => {
   if (!authToken) {
     res.writeHead(401, { "Content-Type": "application/json" });
     res.end(JSON.stringify({ message: "Unauthorized" }));
+    logRequest(req, 401);
     return;
   }
 
@@ -33,8 +35,9 @@ proxyConfig.on("proxyReq", (proxyReq, req, res) => {
   }
 });
 
-proxyConfig.on("proxyRes", (_proxyRes, _req, res) => {
+proxyConfig.on("proxyRes", (proxyRes, req, res) => {
   setCorsHeaders(res, env.PROXY_ALLOWED_ORIGIN);
+  logRequest(req, proxyRes.statusCode);
 });
 
 const server = http.createServer((req, res) => {
@@ -43,6 +46,7 @@ const server = http.createServer((req, res) => {
     setCorsHeaders(res, env.PROXY_ALLOWED_ORIGIN);
     res.writeHead(204);
     res.end();
+    logRequest(req, 204);
     return;
   }
 
@@ -51,5 +55,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(env.PROXY_PORT, () => {
-  console.log(`Proxy server running on port ${env.PROXY_PORT}`);
+  logInfo(`Proxy server running on port ${env.PROXY_PORT}`);
 });
