90a65c6e6740e8ec46b14855378fa71e346cffa9
Rad14nt <andrei@gimli.ro>
2026-01-15 13:41:06 +0100

    run e2e tests parallel

--- client/playwright.config.ts
+++ client/playwright.config.ts
@@ -6,27 +6,22 @@ const baseURL = process.env.BASE_URL || "https://ralphina.ethz.ch";
 
 const ignoreHTTPSErrors = process.env.E2E_IGNORE_HTTPS_ERRORS === "true";
 
-// Output locations (server will set these to mounted /artifacts/...)
 const outputDir = process.env.E2E_OUTPUT_DIR || "test-results";
 const reportDir = process.env.E2E_REPORT_DIR || "playwright-report";
 
-// Reporter behavior:
-// - Locally: html report (nice), but don’t auto-open
-// - CI/server: always html (so nginx can serve it) + also print line output for logs
 const reporters = isCI
     ? [["line"], ["html", { outputFolder: reportDir, open: "never" }]]
     : [["html", { outputFolder: reportDir, open: "never" }]];
 
 export default defineConfig({
     testDir: "tests/e2e",
-    fullyParallel: false,
+    fullyParallel: true,
     forbidOnly: isCI,
     retries: isCI ? 2 : 0,
     workers: isCI ? 1 : undefined,
 
     reporter: reporters,
 
-    // This is where screenshots/videos/traces go
     outputDir,
 
     use: {
