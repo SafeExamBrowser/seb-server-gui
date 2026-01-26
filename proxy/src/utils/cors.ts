765ca3c32b90006ed9497cc40eb00c29e8f4dc08
Alain Horner <alain@hornerdigital.ch>
2026-01-26 12:01:19 +0100

    SEBSERV-832: whitelist fileupload headers

--- proxy/src/utils/cors.ts
+++ proxy/src/utils/cors.ts
@@ -2,14 +2,17 @@ import http from "http";
 
 export const setCorsHeaders = (
   res: http.ServerResponse,
-  allowedOrigin: string
+  allowedOrigin: string,
 ): http.ServerResponse => {
   res.setHeader("Access-Control-Allow-Origin", allowedOrigin);
   res.setHeader(
     "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS",
   );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, importFile, importFilePassword",
+  );
 
   return res;
 };
