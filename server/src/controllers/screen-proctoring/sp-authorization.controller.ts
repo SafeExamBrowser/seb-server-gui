5b892558d3e975d33e9a92d349b056590ac45231
Alain Horner <alain@hornerdigital.ch>
2026-01-27 14:59:42 +0100

    SEBSERV-832: delete old server auth code

--- server/src/controllers/screen-proctoring/sp-authorization.controller.ts
+++ server/src/controllers/screen-proctoring/sp-authorization.controller.ts
@@ -2,44 +2,6 @@ import {Request, Response} from 'express';
 import * as apiService from "../../services/screen-proctoring/sp-api.service";
 import * as authorizationService from '../../services/screen-proctoring/sp-authorization.service';
 
-export async function authorize(req: Request, res: Response){
-
-    try{
-        const username: string = req.body.username;
-        const password: string = req.body.password;
-        const [tokenObject, status] = await authorizationService.authorizeViaScreenProctoringServer(username, password);
-
-        await authorizationService.logLogin("Bearer " + tokenObject.access_token);
-
-        return res.status(status).json(tokenObject);
-
-    }catch(error){
-        apiService.handleGenericApiError(error, res);
-    }
-}
-
-export async function verifyJwt(req: Request, res: Response){
-    try{
-        const token: string = req.body.token;
-        const [tokenObject, status] = await authorizationService.verifyJwt(token);
-
-        return res.status(status).json(tokenObject);
-
-    }catch(error){
-        apiService.handleGenericApiError(error, res);
-    }
-}
-
-export async function refresh(req: Request, res: Response){
-    try{
-        const [tokenObject, status] = await authorizationService.refreshToken(req.headers.authorization.split(" ")[1]);
-        return res.status(status).json(tokenObject);
-
-    }catch(error){
-        apiService.handleGenericApiError(error, res);
-    }
-}
-
 export async function logLogin(req: Request, res: Response){
     try{
         //does not return data
--- server/src/controllers/seb-server/authorization.controller.ts
+++ server/src/controllers/seb-server/authorization.controller.ts
@@ -2,48 +2,6 @@ import {Request, Response} from 'express';
 import * as apiService from "../../services/seb-server/api.service";
 import * as authorizationService from '../../services/seb-server/authorization.service';
 
-export async function authorize(req: Request, res: Response){
-
-    try{
-        const username: string = req.body.username;
-        const password: string = req.body.password;
-        const tokenObject: any = await authorizationService.authorizeViaSebServer(username, password);
-
-        // await authorizationService.logLogin("Bearer " + tokenObject.access_token);
-
-        return res.status(200).json(tokenObject);
-
-    }catch(error){
-        apiService.handleGenericApiError(error, res);
-    }
-}
-
-export async function verifyJwt(req: Request, res: Response){
-
-    try{
-        const token: string = req.body.token;
-
-        const tokenObject: any = await authorizationService.verifyJwt(token);
-
-        return res.status(200).json(tokenObject);
-
-    }catch(error){
-        apiService.handleGenericApiError(error, res);
-    }
-}
-
-export async function refresh(req: Request, res: Response){
-
-    try{
-        const tokenObject: object = await authorizationService.refreshViaSebServer(req.headers.authorization.split(" ")[1]);
-
-        return res.status(200).json(tokenObject);
-
-    }catch(error){
-        apiService.handleGenericApiError(error, res);
-    }
-}
-
 export async function logLogin(req: Request, res: Response){
     try{
         //does not return data
