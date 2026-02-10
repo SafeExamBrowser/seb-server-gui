d9ad66d6c15ad492e32a1b378d5eb5600e8229c2
Alain Horner <alain@hornerdigital.ch>
2026-02-10 12:34:01 +0100

    SEBSERV-832: proxy: rm GET content-type hack

--- proxy/src/index.ts
+++ proxy/src/index.ts
@@ -13,16 +13,6 @@ const proxyConfig = createProxyServer({
   target: `${env.SEB_SERVER_URL}:${env.SEB_SERVER_PORT}${API_PREFIX}`,
 });
 
-proxyConfig.on("proxyReq", (proxyReq, _req, _res) => {
-  if (proxyReq.method === "GET") {
-    // TODO @Andreas: remove this, once the sev-server API doesn't require this anymore
-    // - Content-Type on GET requests doesn't make sense, because GET requests don't have a body
-    // - This leads to the browser filtering the header out if we send it from the client
-    // - Our API (seb-server) however requires a Content-Type, even on GET requests, so we manually add it here
-    proxyReq.setHeader("Content-Type", "application/x-www-form-urlencoded");
-  }
-});
-
 proxyConfig.on("proxyRes", (proxyRes, req, res) => {
   setCorsHeaders(res, env.PROXY_ALLOWED_ORIGIN);
   logRequest(req, proxyRes.statusCode);
