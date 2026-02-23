f0454a297281d090266ff3e74c7089eecc233d47
Alain Horner <alain@hornerdigital.ch>
2026-02-23 12:53:05 +0100

    SEBSERV-832: proxy: whitelist filtering parameter headers for monitoring

--- proxy/src/utils/cors.ts
+++ proxy/src/utils/cors.ts
@@ -1,5 +1,18 @@
 import http from "http";
 
+const ALLOWED_HEADERS_CERTIFICATE = [
+  "importFile",
+  "importFilePassword",
+] as const;
+
+const ALLOWED_HEADERS_MONITORING = [
+  "show-all",
+  "show-states",
+  "show-client-groups",
+  "show-indicators",
+  "show-notifications",
+] as const;
+
 export const setCorsHeaders = (
   res: http.ServerResponse,
   allowedOrigin: string,
@@ -9,9 +22,11 @@ export const setCorsHeaders = (
     "Access-Control-Allow-Methods",
     "GET, POST, PUT, DELETE, PATCH, OPTIONS",
   );
+
+  // TODO @alain: allow additional headers only for the respective endpoints?
   res.setHeader(
     "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, importFile, importFilePassword",
+    `Content-Type, Authorization, ${ALLOWED_HEADERS_CERTIFICATE.join(", ")}, ${ALLOWED_HEADERS_MONITORING.join(", ")}`,
   );
 
   return res;
