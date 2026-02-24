f77701c35d12924b904a1d7518dc180504dcad07
Alain Horner <alain@hornerdigital.ch>
2026-02-24 17:59:12 +0100

    SEBSERV-832: rm unused sp-settings controller and service

--- server/src/routes/routes.ts
+++ server/src/routes/routes.ts
@@ -2,13 +2,10 @@ import express, {Router} from "express";
 
 //screen-proctoring
 import * as groupController from "../controllers/screen-proctoring/sp-group.controller";
-import * as settingsController from "../controllers/screen-proctoring/sp-settings.controller";
 
 const router: Router = express.Router();
 
 //screen-proctoring
-router.get("/sp/settings", settingsController.getSettings)
-
 router.get("/sp/group", groupController.getGroups);
 router.get("/sp/group/:uuid", groupController.getGroupByUuid);
 
