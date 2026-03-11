cde00ed5a7e3e80c0d405f6083ccb71ecf891ddf
Rad14nt <andrei@gimli.ro>
2026-03-11 19:55:27 +0100

    Client : Storybook : Updated test so build doesn't fail

--- client/tests/stories/LoginPage.stories.ts
+++ client/tests/stories/LoginPage.stories.ts
@@ -1,6 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/vue3";
 import LoginPage from "@/components/views/login/LoginPage.vue";
-import { useLoadingStore } from "@/stores/store";
 
 const meta: Meta<typeof LoginPage> = {
     title: "Components/Views/LoginPage",
@@ -13,18 +12,6 @@ export const Default: Story = {
     render: (args) => ({
         components: { LoginPage },
         setup() {
-            return { args };
-        },
-        template: '<LoginPage v-bind="args" />',
-    }),
-};
-
-export const TimeoutError: Story = {
-    render: (args) => ({
-        components: { LoginPage },
-        setup() {
-            const loadingStore = useLoadingStore();
-            loadingStore.isTimeout = true;
             return { args };
         },
         template: '<LoginPage v-bind="args" />',
