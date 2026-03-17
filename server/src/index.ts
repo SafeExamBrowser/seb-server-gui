a3f3d4ada3a4c8907cd4412294a602bcd409c718
Alain Horner <alain@hornerdigital.ch>
2026-03-17 19:26:35 +0100

    SEBSERV-832: simplify proxy backends; remove latest bits of duplicated code

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,68 +1,44 @@
 import express from "express";
-import ProxyServer, { createProxyServer } from "http-proxy-3";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
 import { parseEnv } from "./utils/env.js";
-import { logInfo, logRequest } from "./utils/logger.js";
-import { prepareProxyRequest } from "./utils/prepareProxyRequest.js";
+import { logInfo } from "./utils/logger.js";
+import { createBackend } from "./utils/createBackend.js";
 
-const env = parseEnv();
 const app = express();
+const env = parseEnv();
 
-const clientBuildDirectory = path.join(
-  path.dirname(fileURLToPath(import.meta.url)),
-  "views",
-);
-
-const sebProxy = createProxyServer({
-  target: env.SEB_SERVER_URL,
+const sebProxy = createBackend({
+  baseUrl: env.SEB_SERVER_URL,
+  defaultUrlPrefix: env.SEB_SERVER_DEFAULT_URL,
+  basicAuthUsername: env.SEB_SERVER_USERNAME,
+  basicAuthPassword: env.SEB_SERVER_PASSWORD,
 });
 
-const spsProxy = createProxyServer({
-  target: env.PROCTOR_SERVER_URL,
+const spsProxy = createBackend({
+  baseUrl: env.PROCTOR_SERVER_URL,
+  defaultUrlPrefix: env.PROCTOR_SERVER_DEFAULT_URL,
+  basicAuthUsername: env.PROCTOR_SERVER_USERNAME,
+  basicAuthPassword: env.PROCTOR_SERVER_PASSWORD,
 });
-
-const addProxyHandlers = (proxy: ProxyServer, targetBase: string) => {
-  proxy.on("proxyRes", (proxyRes, req) => {
-    logRequest({
-      method: req.method,
-      url: `${targetBase}${req.url ?? "/"}`,
-      statusCode: proxyRes.statusCode,
-    });
-  });
-};
-
-addProxyHandlers(sebProxy, env.SEB_SERVER_URL);
-addProxyHandlers(spsProxy, env.PROCTOR_SERVER_URL);
 
 // everything that's prefixed with '/api/sps' is proxied to sps
 app.use("/api/sps", (req, res) => {
-  spsProxy.web(
-    prepareProxyRequest({
-      req,
-      defaultUrl: env.PROCTOR_SERVER_DEFAULT_URL,
-      basicAuthUsername: env.PROCTOR_SERVER_USERNAME,
-      basicAuthPassword: env.PROCTOR_SERVER_PASSWORD,
-    }),
-    res,
-  );
+  spsProxy.handleRequest(req, res);
 });
 
 // everything else that's prefixed with '/api' is proxied to seb
 app.use("/api", (req, res) => {
-  sebProxy.web(
-    prepareProxyRequest({
-      req,
-      defaultUrl: env.SEB_SERVER_DEFAULT_URL,
-      basicAuthUsername: env.SEB_SERVER_USERNAME,
-      basicAuthPassword: env.SEB_SERVER_PASSWORD,
-    }),
-    res,
-  );
+  sebProxy.handleRequest(req, res);
 });
 
 // statically serve client if enabled
 if (env.SERVE_CLIENT) {
+  const clientBuildDirectory = path.join(
+    path.dirname(fileURLToPath(import.meta.url)),
+    "views",
+  );
+
   app.use(express.static(clientBuildDirectory));
   app.get("/*path", (_req, res) => {
     res.sendFile(path.join(clientBuildDirectory, "index.html"));
