291f21e851c7fdc15f5e7653a9204fcc7a25677e
Rad14nt <andrei@gimli.ro>
2026-03-18 16:00:38 +0100

    Client : Use Base for for user account store

--- client/src/components/views/seb-server/settings-navigation/user-account/userAccounts/store/userAccountsStore.ts
+++ client/src/components/views/seb-server/settings-navigation/user-account/userAccounts/store/userAccountsStore.ts
@@ -1,7 +1,7 @@
 import { defineStore } from "pinia";
 import type { UserAccount } from "@/models/userAccount";
-import { useDateFilterSettingsStore } from "@/components/views/seb-server/settings-navigation/store/useBaseSettingsStore.ts";
+import { useBaseSettingsStore } from "@/components/views/seb-server/settings-navigation/store/useBaseSettingsStore.ts";
 
 export const useUserAccountsStore = defineStore("userAccount", () => {
-    return useDateFilterSettingsStore<UserAccount>();
+    return useBaseSettingsStore<UserAccount>();
 });
