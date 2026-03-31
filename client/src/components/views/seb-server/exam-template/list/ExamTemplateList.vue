9b19b0bbc98cdad33f91901a5e389273cda9bfd6
Alain Horner <alain@hornerdigital.ch>
2026-03-31 10:11:56 +0200

    SEBSERV-787: basic table

--- client/src/components/views/seb-server/exam-template/list/ExamTemplateList.vue
+++ client/src/components/views/seb-server/exam-template/list/ExamTemplateList.vue
@@ -9,12 +9,7 @@
     >
         <template #PanelMain>
             <LoadingFallbackComponent :loading="isLoading" :error="error">
-                <div
-                    v-for="examTemplate in examTemplates"
-                    :key="examTemplate.id"
-                >
-                    {{ examTemplate.name }}
-                </div>
+                <ExamTemplateTable :headers="headers" :items="examTemplates" />
             </LoadingFallbackComponent>
         </template>
     </BasicPage>
@@ -24,6 +19,7 @@
 import BasicPage from "@/components/layout/pages/BasicPage.vue";
 import { useExamTemplates } from "./composables/useExamTemplates";
 import { computed, onMounted } from "vue";
+import ExamTemplateTable from "./components/ExamTemplateTable.vue";
 
 const {
     data: examTemplatesData,
@@ -33,6 +29,32 @@ const {
 } = useExamTemplates();
 
 const examTemplates = computed(() => examTemplatesData.value?.content ?? []);
+
+// TODO @alain: translate labels
+// TODO @alain: exam type value: use enum translation
+// TODO @alain: add correct action content
+const headers = computed(() => [
+    {
+        title: "Name",
+        value: "name",
+        width: "30%",
+    },
+    {
+        title: "Description",
+        value: "description",
+        width: "30%",
+    },
+    {
+        title: "Exam Type",
+        value: "examType",
+        width: "30%",
+    },
+    {
+        title: "Actions",
+        value: "actions",
+        width: "10%",
+    },
+]);
 
 onMounted(() => {
     fetchData();
---
+++ client/src/components/views/seb-server/exam-template/list/components/ExamTemplateTable.vue
@@ -0,0 +1,13 @@
+<template>
+    <v-data-table :headers="headers" :items="items"></v-data-table>
+</template>
+
+<script setup lang="ts">
+import { ExamTemplate } from "@/models/seb-server/examTemplate";
+import { DataTableHeader } from "vuetify";
+
+defineProps<{
+    items: ExamTemplate[];
+    headers: DataTableHeader<ExamTemplate>[];
+}>();
+</script>
