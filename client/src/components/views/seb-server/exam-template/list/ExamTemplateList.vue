423ddf9aaea10ed2dc184022e4fa673413df7dec
Alain Horner <alain@hornerdigital.ch>
2026-03-31 09:36:19 +0200

    SEBSERV-787: basic list data

--- client/src/components/views/seb-server/exam-template/list/ExamTemplateList.vue
+++ client/src/components/views/seb-server/exam-template/list/ExamTemplateList.vue
@@ -8,11 +8,33 @@
         ]"
     >
         <template #PanelMain>
-            TODO: Implement the exam template list.
+            <LoadingFallbackComponent :loading="isLoading" :error="error">
+                <div
+                    v-for="examTemplate in examTemplates"
+                    :key="examTemplate.id"
+                >
+                    {{ examTemplate.name }}
+                </div>
+            </LoadingFallbackComponent>
         </template>
     </BasicPage>
 </template>
 
 <script setup lang="ts">
 import BasicPage from "@/components/layout/pages/BasicPage.vue";
+import { useExamTemplates } from "./composables/useExamTemplates";
+import { computed, onMounted } from "vue";
+
+const {
+    data: examTemplatesData,
+    loading: isLoading,
+    error,
+    fetchData,
+} = useExamTemplates();
+
+const examTemplates = computed(() => examTemplatesData.value?.content ?? []);
+
+onMounted(() => {
+    fetchData();
+});
 </script>
