a09008bbcae43dbf125b0e92f96a10150b58932e
Rad14nt <andrei@gimli.ro>
2026-04-01 14:54:05 +0200

    Client : Reuse cancel and confirm buttons

--- client/src/components/views/seb-server/settings-navigation/user-account/createUserAccount/CreateUserAccount.vue
+++ client/src/components/views/seb-server/settings-navigation/user-account/createUserAccount/CreateUserAccount.vue
@@ -31,26 +31,8 @@
             <v-row class="px-8 pb-6">
                 <v-col cols="12">
                     <div class="d-flex justify-end">
-                        <v-btn
-                            color="black"
-                            data-testid="createUserAccount-cancel-button"
-                            rounded="sm"
-                            variant="outlined"
-                            @click="navigateTo(constants.USER_ACCOUNTS_ROUTE)"
-                        >
-                            {{ translate("general.cancelButton") }}
-                        </v-btn>
-
-                        <v-btn
-                            class="ml-2"
-                            color="primary"
-                            data-testid="createUserAccount-save-button"
-                            rounded="sm"
-                            variant="flat"
-                            @click="submit"
-                        >
-                            {{ translate("general.saveButton") }}
-                        </v-btn>
+                        <CancelButton text="general.cancelButton" />
+                        <ConfirmButton text="general.saveButton" />
                     </div>
                 </v-col>
             </v-row>
@@ -64,12 +46,11 @@ import { storeToRefs } from "pinia";
 import BasicSettingsPage from "@/components/layout/pages/BasicSettingsPage.vue";
 import HintText from "@/components/views/seb-server/settings-navigation/widgets/HintText.vue";
 import FormBuilder from "@/components/widgets/formBuilder/FormBuilder.vue";
-import { translate } from "@/utils/generalUtils";
-import { navigateTo } from "@/router/navigation";
-import * as constants from "@/utils/constants";
 import { useCreateUserAccountStore } from "./store/useCreateUserAccountStore";
 import { useCreateUserAccountPage } from "@/components/views/seb-server/settings-navigation/user-account/createUserAccount/composable/useCreateUserAccountPage.ts";
 import UserAccountPreviewCard from "@/components/views/seb-server/settings-navigation/widgets/UserAccountPreviewCard.vue";
+import CancelButton from "@/components/views/seb-server/settings-navigation/widgets/CancelButton.vue";
+import ConfirmButton from "@/components/views/seb-server/settings-navigation/widgets/ConfirmButton.vue";
 
 const {
     isFormValid,
