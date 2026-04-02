17b9faddf395d5cab632589e34d556841b44c28b
Rad14nt <andrei@gimli.ro>
2026-04-02 09:48:14 +0200

    Client :  Added disabled to Formbuilder type. This allows for a disabled institution select :). Additionally added defineExpose of validate()  method in Formbuilder. Becauyse the Formbuilder wraps v-form internally, from the outside like for ex Add User Account or future Add forms there was no way to reach inner v form and call vuetify standard validate on it. With this it is now possible.

--- client/src/components/widgets/formBuilder/FormBuilder.vue
+++ client/src/components/widgets/formBuilder/FormBuilder.vue
@@ -1,6 +1,7 @@
 <template>
     <v-form
         :id="formId"
+        ref="formRef"
         @update:model-value="handleModelValueUpdated"
         @submit.prevent="handleSubmit"
     >
@@ -9,11 +10,17 @@
 </template>
 
 <script setup lang="ts">
+import { ref } from "vue";
 import { VForm } from "vuetify/components";
 import { FormFieldsComponentProps } from "./types";
 import FormFields from "./components/FormFields.vue";
 
 const isValid = defineModel<boolean | null>();
+const formRef = ref<InstanceType<typeof VForm>>();
+
+defineExpose({
+    validate: () => formRef.value?.validate(),
+});
 
 defineProps<
     {
