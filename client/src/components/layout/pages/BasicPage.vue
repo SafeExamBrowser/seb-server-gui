8d8a4570a5de1e3d5416967b90aaa6f2b497096e
Rad14nt <andrei@gimli.ro>
2026-05-15 15:36:34 +0200

    Add expand transition to panel top

--- client/src/components/layout/pages/BasicPage.vue
+++ client/src/components/layout/pages/BasicPage.vue
@@ -11,11 +11,13 @@
                 "
             />
         </div>
-        <div v-if="$slots.PanelTop" class="flex-grow-0 flex-shrink-0 pa-2">
-            <v-card elevation="2" rounded="lg">
-                <slot name="PanelTop"></slot>
-            </v-card>
-        </div>
+        <v-expand-transition>
+            <div v-if="$slots.PanelTop" class="flex-grow-0 flex-shrink-0 pa-2">
+                <v-card elevation="2" rounded="lg">
+                    <slot name="PanelTop"></slot>
+                </v-card>
+            </div>
+        </v-expand-transition>
         <v-row class="flex-grow-1 flex-shrink-1" :style="{ minHeight: 0 }">
             <v-col :cols="$slots.PanelAside ? 9 : 12" class="h-100 pa-2">
                 <v-card
