6603d00fd0771ddd98bbefe0eb7e9e82ccfe2fda
Rad14nt <andrei@gimli.ro>
2026-05-15 11:44:12 +0200

    Fix h1 attribute giving too much space by replacing with divs and using new vuetify classes (well they were already used)

--- client/src/components/widgets/SectionTitle.vue
+++ client/src/components/widgets/SectionTitle.vue
@@ -1,7 +1,7 @@
 <template>
-    <h3 class="text-title-large font-weight-bold">
+    <div class="text-title-large font-weight-bold">
         {{ name }}
-    </h3>
+    </div>
 </template>
 
 <script setup lang="ts">
