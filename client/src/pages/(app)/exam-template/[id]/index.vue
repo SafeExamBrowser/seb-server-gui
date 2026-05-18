6f9383e38f6738563ea956ee49bba20bd0d37afb
Alain Horner <alain@hornerdigital.ch>
2026-05-18 10:54:08 +0200

    SEBSERV-788: use basic page layout

--- client/src/pages/(app)/exam-template/[id]/index.vue
+++ client/src/pages/(app)/exam-template/[id]/index.vue
@@ -1,8 +1,21 @@
-<template>Exam Template Detail: {{ examTemplateId }}</template>
+<template>
+    <BasicPage
+        :title="$t('titles.examTemplateDetail')"
+        :bread-crumb="[
+            {
+                label: $t('titles.examTemplateList'),
+                link: { name: '/(app)/exam-template/' },
+            },
+            { label: $t('titles.examTemplateDetail') },
+        ]"
+    >
+        <template #PanelMain> Main content: {{ examTemplateId }} </template>
+    </BasicPage>
+</template>
 
 <script setup lang="ts">
-// TODO @alain: implement
 import { useRoute } from "vue-router";
+import BasicPage from "@/components/layout/pages/BasicPage.vue";
 
 const route = useRoute();
 const examTemplateId = route.params.id;
