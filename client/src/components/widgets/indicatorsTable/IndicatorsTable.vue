6eff2d7745fca7d6b96dfb25f51e4d3a42881c60
Alain Horner <alain@hornerdigital.ch>
2026-05-19 21:42:29 +0200

    SEBSERV-788: cleanup: avoid calling the hook on every rerender

--- client/src/components/widgets/indicatorsTable/IndicatorsTable.vue
+++ client/src/components/widgets/indicatorsTable/IndicatorsTable.vue
@@ -1,5 +1,5 @@
 <template>
-    <CrudTable :config="useTable(deps)">
+    <CrudTable :config="tableConfig">
         <template #item.type="{ item }">
             {{ getTranslatedType(item) }}
         </template>
@@ -30,12 +30,15 @@ import {
     Indicator,
     IndicatorsTableDeps,
 } from "@/components/widgets/indicatorsTable/types.ts";
+import { computed } from "vue";
 
-defineProps<{
+const { deps } = defineProps<{
     deps: IndicatorsTableDeps;
 }>();
 
 const { t } = useI18n();
+
+const tableConfig = computed(() => useTable(deps));
 
 const getTranslatedType = (item: Indicator) =>
     t(`createTemplateExam.steps.indicators.fields.type.types.${item.type}`);
