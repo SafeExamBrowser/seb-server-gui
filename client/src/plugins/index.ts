ce009d1cae8df3ed4ce0f532f2d2992e267f9f12
Rad14nt <andrei@gimli.ro>
2026-05-28 21:10:08 +0200

    Configure QueryClient defaults for production

--- client/src/plugins/index.ts
+++ client/src/plugins/index.ts
@@ -1,9 +1,8 @@
 import pinia from "@/stores";
 import router from "@/router/router";
 import type { App } from "vue";
-import { QueryClient, VueQueryPlugin } from "@tanstack/vue-query";
-
-const queryClient = new QueryClient();
+import { VueQueryPlugin } from "@tanstack/vue-query";
+import { queryClient } from "@/services/http/queryClient";
 
 export function registerPlugins(app: App) {
     app.use(pinia).use(router).use(VueQueryPlugin, { queryClient });
