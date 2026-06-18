23d608fa54f31da7b8627ae4ba2c47a303c9e9cc
Rad14nt <andrei@gimli.ro>
2026-06-18 13:48:36 +0200

    Repoint the institution dropdown, name map and branding at the new services.

--- client/src/composables/useInstitutions.ts
+++ client/src/composables/useInstitutions.ts
@@ -1,5 +1,5 @@
 import { useFetch } from "@/composables/useFetch";
-import { getInstitutions } from "@/services/seb-server/institutionService";
+import { getInstitutions } from "@/services/seb-server/institutionInfoService";
 
 export const useInstitutions = () =>
     useFetch(() => getInstitutions(), { immediate: true });
