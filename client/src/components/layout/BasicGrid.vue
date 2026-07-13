92c7403ac41664ec7564d58ca8599c1b8333e56d
Alain Horner <alain@hornerdigital.ch>
2026-07-13 15:20:49 +0200

    SEBSERV-788: use basic page template for examTemplateDetailPage

---
+++ client/src/components/layout/BasicGrid.vue
@@ -0,0 +1,13 @@
+<template>
+    <v-row>
+        <v-col
+            v-for="(_, name) in $slots"
+            :key="name"
+            cols="12"
+            md="6"
+            class="d-flex flex-column"
+        >
+            <slot :name="name" />
+        </v-col>
+    </v-row>
+</template>
--- client/src/pages/(app)/exam-template/[id]/index.vue
+++ client/src/pages/(app)/exam-template/[id]/index.vue
@@ -1,58 +1,66 @@
 <template>
-    <LoadingFallbackComponent :loading="loading" :errors="errors">
-        <GridPage
-            v-if="examTemplateId !== undefined"
-            :title="title"
-            :bread-crumb="breadCrumb"
-        >
-            <template #basicSettings>
-                <BoxBasicSettings
-                    :basic-settings="basicSettings"
-                    @change="handleBasicSettingsChange"
-                />
-            </template>
-            <template #sebSettings>
-                <BoxSEBSettings
-                    v-if="basicSettings.configurationTemplateId"
-                    :exam-template-name="basicSettings.name"
-                    :config-template-id="basicSettings.configurationTemplateId"
-                />
-            </template>
-            <template #indicators>
-                <BoxIndicators
-                    :exam-template-id="examTemplateId"
-                    :indicators="indicators"
-                />
-            </template>
-            <template #supervisors>
-                <BoxSupervisors
-                    :available-supervisors="availableSupervisors ?? []"
-                    :selected-supervisor-ids="selectedSupervisorIds"
-                    @change="handleSupervisorsChange"
-                />
-            </template>
-            <template #screenProctoringSettings>
-                <BoxScreenProctoringSettings
-                    :enabled="screenProctoring.enabled.value"
-                    :collection-strategy="
-                        screenProctoring.collectionStrategy.value
-                    "
-                    @change="handleScreenProctoringChange"
-                />
-            </template>
-            <template #groups>
-                <BoxClientGroups
-                    :exam-template-id="examTemplateId"
-                    :client-groups="clientGroups"
-                    :screen-proctoring="screenProctoring"
-                />
-            </template>
-        </GridPage>
-    </LoadingFallbackComponent>
+    <BasicPage
+        floating
+        :title="title"
+        :bread-crumb="breadCrumb"
+        :data-test-id="dataTestId"
+    >
+        <template #PanelMain>
+            <LoadingFallbackComponent :loading="loading" :errors="errors">
+                <BasicGrid v-if="examTemplateId !== undefined">
+                    <template #basicSettings>
+                        <BoxBasicSettings
+                            :basic-settings="basicSettings"
+                            @change="handleBasicSettingsChange"
+                        />
+                    </template>
+                    <template #sebSettings>
+                        <BoxSEBSettings
+                            v-if="basicSettings.configurationTemplateId"
+                            :exam-template-name="basicSettings.name"
+                            :config-template-id="
+                                basicSettings.configurationTemplateId
+                            "
+                        />
+                    </template>
+                    <template #indicators>
+                        <BoxIndicators
+                            :exam-template-id="examTemplateId"
+                            :indicators="indicators"
+                        />
+                    </template>
+                    <template #supervisors>
+                        <BoxSupervisors
+                            :available-supervisors="availableSupervisors ?? []"
+                            :selected-supervisor-ids="selectedSupervisorIds"
+                            @change="handleSupervisorsChange"
+                        />
+                    </template>
+                    <template #screenProctoringSettings>
+                        <BoxScreenProctoringSettings
+                            :enabled="screenProctoring.enabled.value"
+                            :collection-strategy="
+                                screenProctoring.collectionStrategy.value
+                            "
+                            @change="handleScreenProctoringChange"
+                        />
+                    </template>
+                    <template #groups>
+                        <BoxClientGroups
+                            :exam-template-id="examTemplateId"
+                            :client-groups="clientGroups"
+                            :screen-proctoring="screenProctoring"
+                        />
+                    </template>
+                </BasicGrid>
+            </LoadingFallbackComponent>
+        </template>
+    </BasicPage>
 </template>
 
 <script setup lang="ts">
-import GridPage from "@/components/layout/pages/GridPage.vue";
+import BasicPage from "@/components/layout/pages/BasicPage.vue";
+import BasicGrid from "@/components/layout/BasicGrid.vue";
 import LoadingFallbackComponent from "@/components/widgets/loadingFallbackComponent/LoadingFallbackComponent.vue";
 import { useExamTemplateDetailPage } from "./composables/useExamTemplateDetailPage.ts";
 import BoxBasicSettings from "@/pages/(app)/exam-template/[id]/components/BoxBasicSettings/BoxBasicSettings.vue";
@@ -61,6 +69,15 @@ import BoxIndicators from "./components/BoxIndicators.vue";
 import BoxSupervisors from "./components/BoxSupervisors.vue";
 import BoxScreenProctoringSettings from "./components/BoxScreenProctoringSettings/BoxScreenProctoringSettings.vue";
 import BoxClientGroups from "./components/BoxClientGroups.vue";
+
+definePage({
+    meta: {
+        titleKey: "titles.examTemplateDetail",
+        pageTestId: "exam-template-detail-page",
+    },
+});
+
+const dataTestId = "examTemplateDetail";
 
 const {
     examTemplateId,
