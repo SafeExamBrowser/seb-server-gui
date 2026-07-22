bf60bf12af14af7b91317032bc005bc9060d77e4
Rad14nt <andrei@gimli.ro>
2026-07-22 16:23:09 +0300

    general: remove the legacy error store and its snackbar
    Every API failure is already toasted globally by the axios interceptor through the notification queue, and the ErrorMsg message table had no remaining producers. Drops the error store, the ErrorMsg widget, its App.vue mount and the orphaned ErrorProps type.

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,6 +1,5 @@
 <template>
     <v-app>
-        <ErrorMsg></ErrorMsg>
         <RouterView />
         <NotificationQueue />
     </v-app>
@@ -12,7 +11,6 @@ import "@vuepic/vue-datepicker/dist/main.css";
 import { onMounted } from "vue";
 import { RouterView } from "vue-router";
 
-import ErrorMsg from "@/components/widgets/ErrorMsg.vue";
 import NotificationQueue from "@/components/widgets/notifications/NotificationQueue.vue";
 import { useTokenRefresh } from "@/composables/useTokenRefresh";
 
