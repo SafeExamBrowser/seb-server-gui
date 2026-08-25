418a5b9c25d74a5f494fadee102dfae9319ea07c
Alain Horner <alain@hornerdigital.ch>
2026-08-25 10:03:36 +0200

    #SEBSERV-968: remove copy-from-template machinery from the exam Groups box

--- client/src/pages/(app)/exam/[id]/components/BoxClientGroups/BoxClientGroups.vue
+++ client/src/pages/(app)/exam/[id]/components/BoxClientGroups/BoxClientGroups.vue
@@ -1,39 +1,5 @@
 <template>
     <DetailBox :title="$t('examDetail.boxes.clientGroups.title')">
-        <template #action>
-            <v-menu @update:model-value="handleMenuToggle">
-                <template #activator="{ props: menuProps }">
-                    <BoxActionButton
-                        v-if="!editHidden"
-                        v-bind="menuProps"
-                        icon="mdi-plus-circle-outline"
-                        :label="$t('examDetail.boxes.clientGroups.addButton')"
-                        :disabled="editDisabled"
-                    />
-                </template>
-
-                <v-card min-width="280" max-width="400" max-height="400">
-                    <v-progress-linear v-if="templateLoading" indeterminate />
-                    <v-card-text v-else-if="templateErrorMessage">
-                        {{ templateErrorMessage }}
-                    </v-card-text>
-                    <v-card-text v-else-if="templateGroups.length === 0">
-                        {{ $t("examDetail.boxes.clientGroups.popover.empty") }}
-                    </v-card-text>
-                    <v-list v-else>
-                        <v-list-item
-                            v-for="templateGroup in templateGroups"
-                            :key="templateGroup.id"
-                            :title="templateGroup.name"
-                            :subtitle="getClientGroupTypeDetails(templateGroup)"
-                            :disabled="copyLoading"
-                            @click="handleCopyClick(templateGroup)"
-                        />
-                    </v-list>
-                </v-card>
-            </v-menu>
-        </template>
-
         <LoadingFallbackComponent :loading="loading" :errors="boxErrors">
             <v-data-table
                 :headers="headers"
@@ -79,28 +45,14 @@
 <script setup lang="ts">
 import { computed, ref } from "vue";
 import { useI18n } from "vue-i18n";
-import {
-    VBtn,
-    VCard,
-    VCardText,
-    VDataTable,
-    VList,
-    VListItem,
-    VMenu,
-    VProgressLinear,
-} from "vuetify/components";
+import { VBtn, VDataTable } from "vuetify/components";
 
-import BoxActionButton from "@/components/widgets/BoxActionButton.vue";
 import DeleteConfirmDialog from "@/components/widgets/confirmDialog/DeleteConfirmDialog.vue";
 import DetailBox from "@/components/widgets/DetailBox.vue";
 import LoadingFallbackComponent from "@/components/widgets/loadingFallbackComponent/LoadingFallbackComponent.vue";
 import { ClientGroup } from "@/models/seb-server/clientGroup.ts";
 import { Exam } from "@/models/seb-server/exam.ts";
-import { ClientGroupExisting } from "@/models/seb-server/examTemplate.ts";
-import {
-    getClientGroupTypeDetails,
-    getExamClientGroupTypeDetails,
-} from "@/utils/clientGroup.ts";
+import { getExamClientGroupTypeDetails } from "@/utils/clientGroup.ts";
 
 import { useClientGroupsBox } from "./composables/useClientGroupsBox.ts";
 
@@ -119,12 +71,6 @@ const {
     error,
     editHidden,
     editDisabled,
-    templateGroups,
-    templateLoading,
-    templateErrorMessage,
-    loadTemplateGroups,
-    copyTemplateGroup,
-    copyLoading,
     deleteGroup,
     deleteLoading,
 } = useClientGroupsBox(examId, examRef);
@@ -149,16 +95,6 @@ const headers = computed(() => [
         align: "end" as const,
     },
 ]);
-
-const handleMenuToggle = (open: boolean) => {
-    if (open) {
-        loadTemplateGroups();
-    }
-};
-
-const handleCopyClick = async (templateGroup: ClientGroupExisting) => {
-    await copyTemplateGroup(templateGroup);
-};
 
 const deleteDialogOpen = ref(false);
 const deleteTarget = ref<ClientGroup>();
--- client/src/pages/(app)/exam/[id]/components/BoxClientGroups/composables/useClientGroupsBox.ts
+++ client/src/pages/(app)/exam/[id]/components/BoxClientGroups/composables/useClientGroupsBox.ts
@@ -1,4 +1,3 @@
-import { useMutation as useTanstackMutation } from "@tanstack/vue-query";
 import { computed, type Ref } from "vue";
 
 import { useFetch } from "@/composables/useFetch.ts";
@@ -6,18 +5,10 @@ import { useMutation } from "@/composables/useMutation.ts";
 import i18n from "@/i18n";
 import { ClientGroup } from "@/models/seb-server/clientGroup.ts";
 import { Exam } from "@/models/seb-server/exam.ts";
-import { ClientGroupExisting } from "@/models/seb-server/examTemplate.ts";
-import { templateGroupToClientGroup } from "@/pages/(app)/exam/[id]/components/BoxClientGroups/utils/templateGroupToClientGroup.ts";
 import { useExamActionAccess } from "@/pages/(app)/exam/[id]/composables/useExamActionAccess.ts";
 import { GUIAction } from "@/services/ability.ts";
-import {
-    appErrorToMessage,
-    isNotFoundError,
-    toAppErrorOrUndefined,
-} from "@/services/errors/toAppError.ts";
 import { notify } from "@/services/notifications/notify.ts";
 import * as clientGroupService from "@/services/seb-server/clientGroupService.ts";
-import { getExamTemplateSelectionById } from "@/services/seb-server/examTemplateService.ts";
 
 export const useClientGroupsBox = (
     examId: number,
@@ -34,67 +25,6 @@ export const useClientGroupsBox = (
         exam,
         GUIAction.EDIT_CLIENT_GROUPS,
     );
-
-    const templateFetch = useTanstackMutation({
-        mutationFn: (modelId: string) => getExamTemplateSelectionById(modelId),
-    });
-    const templateError = computed(() =>
-        toAppErrorOrUndefined(templateFetch.error.value),
-    );
-
-    // a failed fetch must show the empty state, not the previous template's groups
-    const templateGroups = computed(() => {
-        if (templateError.value) {
-            return [];
-        }
-
-        return templateFetch.data.value?.CLIENT_GROUP_TEMPLATES ?? [];
-    });
-
-    // A deleted template (404) is a regular empty state, not an error
-    const templateNotFound = computed(() =>
-        isNotFoundError(templateError.value),
-    );
-
-    const templateErrorMessage = computed(() =>
-        templateNotFound.value || !templateError.value
-            ? undefined
-            : appErrorToMessage(templateError.value),
-    );
-
-    const loadTemplateGroups = () => {
-        const templateId = exam.value?.examTemplateId;
-
-        if (templateId === undefined) {
-            return;
-        }
-
-        void templateFetch
-            .mutateAsync(String(templateId))
-            .catch(() => undefined);
-    };
-
-    const copyMutation = useMutation((templateGroup: ClientGroupExisting) =>
-        clientGroupService.createClientGroup(
-            templateGroupToClientGroup(examId, templateGroup),
-        ),
-    );
-
-    const copyTemplateGroup = async (templateGroup: ClientGroupExisting) => {
-        await copyMutation.mutateData(templateGroup);
-
-        if (copyMutation.error.value) {
-            notify.serverError(copyMutation.error.value, {
-                titleOverride: i18n.global.t(
-                    "examDetail.boxes.clientGroups.errors.copyFailed",
-                ),
-            });
-
-            return;
-        }
-
-        await groupsFetch.fetchData();
-    };
 
     const deleteMutation = useMutation((clientGroupId: number) =>
         clientGroupService.deleteClientGroup(String(clientGroupId)),
@@ -126,12 +56,6 @@ export const useClientGroupsBox = (
         error: groupsFetch.error,
         editHidden,
         editDisabled,
-        templateGroups,
-        templateLoading: templateFetch.isPending,
-        templateErrorMessage,
-        loadTemplateGroups,
-        copyTemplateGroup,
-        copyLoading: copyMutation.loading,
         deleteGroup,
         deleteLoading: deleteMutation.loading,
     };
