49f439723b5d893d70dea571ca78534b718be844
Rad14nt <andrei@gimli.ro>
2026-09-04 10:47:15 +0200

    Readd exam template created from name

--- client/src/pages/(app)/exam/[id]/components/ExamSidePanel/components/ExamSidePanelInfo.vue
+++ client/src/pages/(app)/exam/[id]/components/ExamSidePanel/components/ExamSidePanelInfo.vue
@@ -14,12 +14,27 @@
             {{ statusLabel }}
         </v-chip>
     </div>
+
+    <div
+        v-if="examTemplateName"
+        class="d-flex flex-column ga-1 mt-2 align-start"
+    >
+        <span
+            class="text-body-small text-medium-emphasis text-uppercase font-weight-bold"
+        >
+            {{ $t("examDetail.sidePanel.createdWithTemplate") }}
+        </span>
+        <span class="text-primary font-weight-medium text-decoration-none">
+            {{ examTemplateName }}
+        </span>
+    </div>
 </template>
 
 <script setup lang="ts">
 import { computed } from "vue";
 import { VChip } from "vuetify/components";
 
+import { useExamTemplateNames } from "@/composables/useExamTemplateNames.ts";
 import { Exam } from "@/models/seb-server/exam.ts";
 import { ExamStatusEnum } from "@/models/seb-server/examFiltersEnum.ts";
 import * as generalUtils from "@/utils/generalUtils.ts";
@@ -27,6 +42,16 @@ import * as generalUtils from "@/utils/generalUtils.ts";
 const props = defineProps<{
     exam?: Exam;
 }>();
+
+const { data: examTemplateNames } = useExamTemplateNames();
+
+const examTemplateName = computed(
+    () =>
+        examTemplateNames.value?.find(
+            (templateName) =>
+                templateName.modelId === String(props.exam?.examTemplateId),
+        )?.name,
+);
 
 const statusEnum = computed(() =>
     generalUtils.findEnumValue(ExamStatusEnum, props.exam?.status),
