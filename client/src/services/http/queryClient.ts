a6669fedfee619828797743d45efbf4e138bfa88
Alain Horner <alain@hornerdigital.ch>
2026-09-14 19:01:42 +0200

    handover: refactoring notes for andrei

--- client/src/services/http/queryClient.ts
+++ client/src/services/http/queryClient.ts
@@ -1,5 +1,9 @@
 import { QueryClient } from "@tanstack/vue-query";
 
+// TODO @andrei: consider using the TanStackQuery default options here
+// - they are generally chosen for a good reason
+// - deviating from them creates more problems than it solves
+// - if anything, the options should only be overridden for specific queries, not globally
 export const queryClient = new QueryClient({
     defaultOptions: {
         queries: {
